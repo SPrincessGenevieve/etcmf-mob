fa9f1757609b779d1dd9dd3dda5469d0f3eb2778
sprincessgenevieve <sprincessgenevieve@gmail.com>
2025-03-16 12:04:29 +0800

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View className='bg-[red]'>
+      <Text className='text-[40px]'>HELLO</Text>
+    </View>
+  );
+}
